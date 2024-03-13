a2302136e273d11332661c02909bd9dad7ab2fc4
wjjwkwindy <wjjwkwindy@gmail.com>
2024-03-13 11:45:45 +0800

    test: Update exanple

--- example/index.js
+++ example/index.js
@@ -1,2 +1,3 @@
 Floaty();
-Floaty({ speed:100,statuX: 1, statuY: 1, randomStatus: false, x: 10, y: 10, randomPosition: false, closeButton: false, img: 'https://placehold.co/300x300/000/fff' });
+Floaty({ speed: 100, statuX: 1, statuY: 1, randomStatus: false, x: 10, y: 10, randomPosition: false, closeButton: false, img: 'https://placehold.co/300x300/000/fff', url: 'https://www.example.com' });
+Floaty({closeButtonPosition:'outer',url:'https://www.baidu.com'})
