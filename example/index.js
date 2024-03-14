93876ffc6f86a4e8b12aa8e49524891eb07ca539
wjjwkwindy <wjjwkwindy@gmail.com>
2024-03-14 17:15:14 +0800

    style: update style

--- example/index.js
+++ example/index.js
@@ -1,3 +1,19 @@
 Floaty();
-Floaty({ speed: 100, statuX: 1, statuY: 1, randomStatus: false, x: 10, y: 10, randomPosition: false, closeButton: false, img: 'https://placehold.co/300x300/000/fff', url: 'https://www.example.com' });
-Floaty({ closeButtonPosition: 'outer', url: 'https://www.example.com' });
+
+Floaty({
+  speed: 100,
+  statuX: 1,
+  statuY: 1,
+  randomStatus: false,
+  x: 10,
+  y: 10,
+  randomPosition: false,
+  closeButton: false,
+  img: 'https://placehold.co/300x300/000/fff',
+  url: 'https://www.example.com'
+});
+
+Floaty({
+  closeButtonPosition: 'outer',
+  url: 'https://www.example.com'
+});
